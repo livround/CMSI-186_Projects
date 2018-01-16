b1fac6fc02bdd15fd0482d7ab6d25d05864fa3cd
livround <35271510+livround@users.noreply.github.com>
2018-01-15 22:38:50 -0800

    Greeter
    Running 'hello, world' with Java

---
+++ Homework00/Greeter.java
@@ -0,0 +1,15 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* File name     :  Greeter
+* Purpose       :  Alerts "Hello, World!"  
+* Author        :  Olivia Round
+* Date          :  1/15/18
+* Notes         :  None 
+* Warnings      :  None
+* Exception     :  None
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+public class Greeter {
+     public static void main( String[] args ) {
+          System.out.println( “Hello, world!” );
+     }
+}
+
