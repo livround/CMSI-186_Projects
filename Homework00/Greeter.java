d808e5745099251fd28814575bac5ecc97b6c4f6
livround <35271510+livround@users.noreply.github.com>
2018-01-16 09:35:54 -0800

    Update Greeter.java

--- Homework00/Greeter.java
+++ Homework00/Greeter.java
@@ -9,7 +9,7 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 public class Greeter {
      public static void main( String[] args ) {
-          System.out.println( “Hello, world!” );
+          System.out.println( "Hello, world!" );
      }
 }
 
